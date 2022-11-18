9db7e4981cfabe4cd95adfce96b9a7493a16dd26
Ridicu1As <ridicu1As42@gmail.com>
2022-11-18 15:55:26 -0600

    Initial Commit

---
+++ SwiftLearning/SwiftLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLearningApp.swift
+//  SwiftLearning
+//
+//  Created by Adam Cubas on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
